5a11c455e32be056d0296ba1449839b78faae927
sidin <officialzedro@gmail.com>
2023-04-19 23:09:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Counter from "./components/Counter";
+
+import  {useState} from 'react';
+function App() {
+   const [count, setCount] = useState(0);
+
+   const onIncrement = () => {
+      if (count >= 0 && count < 10) {
+         setCount((prev)=>prev+1);
+      }
+   };
+   const onDecrement = () => {
+      if (count > 0) {
+         setCount((prev)=>prev-1);
+      }
+   };
+   return (
+      <div className="App">
+         <Counter count={count} onIncrement={onIncrement} onDecrement={onDecrement} />
+      </div>
+   );
+}
+
+export default App;
